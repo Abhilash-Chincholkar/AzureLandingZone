b742997eb0d1f05593b5df201c94551e3d411d8e
Abhilash-Chincholkar <abhilashchincholkar@gmail.com>
2025-09-16 21:36:47 +0530

    add modules

--- Env/dev/main.tf
+++ Env/dev/main.tf
@@ -4,96 +4,96 @@ module "resource_group" {
   location = "Switzerland North"
 }
 
-// module "virtual_network" {
-//   depends_on              = [module.resource_group]
-//   source                  = "../../modules/azurerm_virtual_network"
-//   virtual_network_name    = "vnet_lb"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-//   address_space           = ["10.0.0.0/16"]
-// }
+module "virtual_network" {
+  depends_on              = [module.resource_group]
+  source                  = "../../modules/azurerm_virtual_network"
+  virtual_network_name    = "vnet_lb"
+  resource_group_name     = "resource_group_lb"
+  resource_group_location = "Switzerland North"
+  address_space           = ["10.0.0.0/16"]
+}
 
-// module "subnet" {
-//   depends_on           = [module.virtual_network]
-//   source               = "../../modules/azurerm_subnet"
-//   subnet_name          = "subnet_lb"
-//   resource_group_name  = "resource_group_lb"
-//   virtual_network_name = "vnet_lb"
-//   address_prefixes     = ["10.0.1.0/24"]
-// }
+module "subnet" {
+  depends_on           = [module.virtual_network]
+  source               = "../../modules/azurerm_subnet"
+  subnet_name          = "subnet_lb"
+  resource_group_name  = "resource_group_lb"
+  virtual_network_name = "vnet_lb"
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-// module "public_ip" {
-//   depends_on              = [module.resource_group]
-//   source                  = "../../modules/azurerm_public_ip"
-//   public_ip               = "LoadBalancerIP"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-// }
+module "public_ip" {
+  depends_on              = [module.resource_group]
+  source                  = "../../modules/azurerm_public_ip"
+  public_ip               = "LoadBalancerIP"
+  resource_group_name     = "resource_group_lb"
+  resource_group_location = "Switzerland North"
+}
 
-// module "virtual_machine_1" {
-//   depends_on              = [module.subnet]
-//   source                  = "../../modules/azurerm_linux_virtual_machine"
-//   subnet_name             = "subnet_lb"
-//   virtual_network_name    = "vnet_lb"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-//   virtual_machine_name    = "vm1"
-//   size                    = "Standard_B2ms"
-//   nic_name                = "lbnic1"
-// }
+module "virtual_machine_1" {
+  depends_on              = [module.subnet]
+  source                  = "../../modules/azurerm_linux_virtual_machine"
+  subnet_name             = "subnet_lb"
+  virtual_network_name    = "vnet_lb"
+  resource_group_name     = "resource_group_lb"
+  resource_group_location = "Switzerland North"
+  virtual_machine_name    = "vm1"
+  size                    = "Standard_B2ms"
+  nic_name                = "lbnic1"
+}
 
-// module "virtual_machine_2" {
-//   depends_on              = [module.subnet]
-//   source                  = "../../modules/azurerm_linux_virtual_machine"
-//   subnet_name             = "subnet_lb"
-//   virtual_network_name    = "vnet_lb"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-//   virtual_machine_name    = "vm2"
-//   size                    = "Standard_B2ms"
-//   nic_name                = "lbnic2"
-// }
+module "virtual_machine_2" {
+  depends_on              = [module.subnet]
+  source                  = "../../modules/azurerm_linux_virtual_machine"
+  subnet_name             = "subnet_lb"
+  virtual_network_name    = "vnet_lb"
+  resource_group_name     = "resource_group_lb"
+  resource_group_location = "Switzerland North"
+  virtual_machine_name    = "vm2"
+  size                    = "Standard_B2ms"
+  nic_name                = "lbnic2"
+}
 
-// module "loadbalancer" {
-//   depends_on              = [module.public_ip, module.virtual_machine_1, module.virtual_machine_2]
-//   source                  = "../../modules/azurerm_load_balancer"
-//   loadbalancer_name       = "TestLoadBalancer"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-// }
+# module "loadbalancer" {
+#   depends_on              = [module.public_ip, module.virtual_machine_1, module.virtual_machine_2]
+#   source                  = "../../modules/azurerm_load_balancer"
+#   loadbalancer_name       = "TestLoadBalancer"
+#   resource_group_name     = "resource_group_lb"
+#   resource_group_location = "Switzerland North"
+# }
 
-// module "azurebastion_subnet" {
-//   depends_on           = [module.virtual_network]
-//   source               = "../../modules/azurerm_subnet"
-//   subnet_name          = "AzureBastionSubnet"
-//   resource_group_name  = "resource_group_lb"
-//   virtual_network_name = "vnet_lb"
-//   address_prefixes     = ["10.0.2.0/26"]
-// }
+module "azurebastion_subnet" {
+  depends_on           = [module.virtual_network]
+  source               = "../../modules/azurerm_subnet"
+  subnet_name          = "AzureBastionSubnet"
+  resource_group_name  = "resource_group_lb"
+  virtual_network_name = "vnet_lb"
+  address_prefixes     = ["10.0.2.0/26"]
+}
 
-// module "public_ip_bastion" {
-//   depends_on              = [module.resource_group]
-//   source                  = "../../modules/azurerm_public_ip"
-//   public_ip               = "BastionIP"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-// }
+module "public_ip_bastion" {
+  depends_on              = [module.resource_group]
+  source                  = "../../modules/azurerm_public_ip"
+  public_ip               = "BastionIP"
+  resource_group_name     = "resource_group_lb"
+  resource_group_location = "Switzerland North"
+}
 
-// module "bastion" {
-//   source                  = "../../modules/azurerm_bastion"
-//   depends_on              = [module.azurebastion_subnet, module.public_ip_bastion]
-//   bastion_name            = "Bastion"
-//   resource_group_name     = "resource_group_lb"
-//   resource_group_location = "Switzerland North"
-//   subnet_name             = "AzureBastionSubnet"
-//   virtual_network_name    = "vnet_lb"
-//   public_ip_name          = "BastionIP"
-// }
+module "bastion" {
+  source                  = "../../modules/azurerm_bastion"
+  depends_on              = [module.azurebastion_subnet, module.public_ip_bastion]
+  bastion_name            = "Bastion"
+  resource_group_name     = "resource_group_lb"
+  resource_group_location = "Switzerland North"
+  subnet_name             = "AzureBastionSubnet"
+  virtual_network_name    = "vnet_lb"
+  public_ip_name          = "BastionIP"
+}
 
-// module "nsg" {
-//   source               = "../../modules/azurerm_network_security_groups"
-//   depends_on           = [module.subnet]
-//   subnet_name          = "subnet_lb"
-//   virtual_network_name = "vnet_lb"
-//   resource_group_name  = "resource_group_lb"
-// }
+module "nsg" {
+  source               = "../../modules/azurerm_network_security_groups"
+  depends_on           = [module.subnet]
+  subnet_name          = "subnet_lb"
+  virtual_network_name = "vnet_lb"
+  resource_group_name  = "resource_group_lb"
+}
